80ede480baf489124ea55003d2840083e5c4a859
Jamie Webster <jamie_maxwell_webster@hotmail.com>
2017-04-15 09:09:51 -0400

    Added some basic tests

---
+++ tests/HGV.Orchid.Test/BasicTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SteamKit2;
+using HGV.Crystalys;
+using HGV.Orchid.Controllers;
+
+namespace HGV.Orchid.Test
+{
+    [TestClass]
+    public class BasicTests
+    {
+        [TestMethod]
+        public void GetMatchMeta()
+        {
+            SteamDirectory.Initialize().Wait();
+
+            var gameClient = new DotaClient("Thantsking", "aPhan3sah");
+            gameClient.Connect();
+
+            var controller = new MatchController(gameClient);
+            var match = controller.Get(3111014659);
+
+            Assert.IsNotNull(match);
+        }
+    }
+}
--- tests/HGV.Orchid.Test/Properties/AssemblyInfo.cs
+++ tests/HGV.Orchid.Test/Properties/AssemblyInfo.cs
@@ -2,18 +2,18 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("HGV.Orchid.Tests")]
+[assembly: AssemblyTitle("HGV.Orchid.Test")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("HGV.Orchid.Tests")]
+[assembly: AssemblyProduct("HGV.Orchid.Test")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: Guid("0b5c6d63-507c-4a15-8049-63e3be0747b3")]
+[assembly: Guid("82d790bd-7572-4b1d-a05e-2d5b19755dd1")]
 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
