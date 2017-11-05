0c8535988d8630e1ca14184f811838907e522ca6
Jamie Webster <jamie_maxwell_webster@hotmail.com>
2017-11-05 13:56:35 -0500

    updates tests

--- tests/BasicTests.cs
+++ tests/BasicTests.cs
@@ -3,18 +3,29 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SteamKit2;
 using HGV.Crystalys;
 using HGV.Orchid.Controllers;
+using System.Configuration;
 
 namespace HGV.Orchid.Test
 {
     [TestClass]
     public class BasicTests
     {
+        private UserInfo GetUserInfo()
+        {
+            return new UserInfo()
+            {
+                Username = ConfigurationManager.AppSettings["Steam:Username"], 
+                Password = ConfigurationManager.AppSettings["Steam:Password"] 
+            };
+        }
+
         [TestMethod]
         public void GetMatchMeta()
         {
             SteamDirectory.Initialize().Wait();
 
-            var gameClient = new DotaClient("Thantsking", "aPhan3sah");
+            var userinfo = GetUserInfo();
+            var gameClient = new DotaClient(userinfo.Username, userinfo.Password);
             gameClient.Connect();
 
             var controller = new MatchController(gameClient);
